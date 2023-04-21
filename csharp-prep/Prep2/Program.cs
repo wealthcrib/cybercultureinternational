d39785b406ab3f08a6cd9adb600ea27819250c65
wealthcrib <eagleajanaku1@gmail.com>
2023-04-21 20:45:53 +0100

    grade

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Enter your grade percentage: ");
+        string answer = Console.ReadLine();
+        int percent = int.Parse(answer);
+
+        string letter = "";
+
+        if (percent >= 90)
+        {
+            letter = "A";
+        }
+        else if (percent >= 80)
+        {
+            letter = "B";
+        }
+        else if (percent >= 70)
+        {
+            letter = "C";
+        }
+        else if (percent >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"{letter} is your grade");
+        
+        if (percent >= 70)
+        {
+            Console.WriteLine(" Congratulations You passed!");
+        }
+        else
+        {
+            Console.WriteLine("Try next time!");
+        }
     }
 }
