32be531a47b24935738180737be62b2d8106cd71
wealthcrib <eagleajanaku1@gmail.com>
2023-04-26 21:01:01 +0100

    p3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,31 @@
-using System;
+ using System;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+         Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 50);
+
+        int guess = -1;
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (magicNumber > guess)
+            {
+                Console.WriteLine("Guess Higher");
+            }
+            else if (magicNumber < guess)
+            {
+                Console.WriteLine("Guess Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+
+        }
     }
 }
